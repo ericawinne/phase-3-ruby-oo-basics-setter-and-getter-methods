65d6e6cdb7c599127531014ba39550d0bb7df1f7
Erica Winne <erica.winne@powerhrg.com>
2021-08-09 20:11:20 -0400

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,46 @@
 class Dog
+  def name
+    @name
+  end
 
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
 end
+
+
+
+
+#below is what I tried first, didnt pass 
+
+# class Dog
+#   def name=(name)
+#     @name
+#   end
+
+#   def name
+#     @name
+#   end
+
+# name = Dog.new
+# dog.name = "Benny"
+
+#   def breed=(breed)
+#     @breed
+#   end
+
+#   def breed 
+#     @breed
+#   end
+# end
+
+# breed = Dog.new
+# pug.breed = "Pug"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,44 @@
 class Person
+  def name
+    @name
+  end
 
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
 end
+
+
+# class Person
+#   def name=(name)
+#     @name = name 
+#   end
+
+#   def name
+#     @name
+#   end
+# end
+
+# kanye = Person.new
+# kanye.name = "Kanye" 
+
+# class Person
+#   def name=(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+# end
+
+# erica = Person.new
+# erica.name = "Erica"
